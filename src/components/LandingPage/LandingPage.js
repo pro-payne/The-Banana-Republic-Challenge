d6c637945e1ff53d551252d24e5779154522b144
Ayabonga Payne <ayabongapayne@gmail.com>
2023-09-13 12:09:01 +0200

    unused removed

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faChevronCircleRight,
   faChevronCircleLeft,
   faUserCircle,
 } from "@fortawesome/free-solid-svg-icons";
